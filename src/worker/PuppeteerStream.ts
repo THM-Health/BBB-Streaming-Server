2215c789ff79802b0386d3abf28ce23d1ed29edc
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-02-07 13:47:26 +0100

    Add LICENSE

--- src/worker/PuppeteerStream.ts
+++ src/worker/PuppeteerStream.ts
@@ -1,3 +1,35 @@
+/**
+ * Attribution and Modifications Notice:
+ * 
+ * This file is based on the original source code by Flam3rboy (https://github.com/SamuelScheit/puppeteer-stream/blob/f5c13e8291011be0fda350a78978a355f5cbc7b7/src/PuppeteerStream.ts).
+ * Modifications have been made to adapt the code for the BBB-Streaming-Server project.
+ * Notable changes include the addition of options to mute and unmute the audio.
+ *
+ * ---- Original License Notice ----
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2022 Flam3rboy
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import {
 	launch as puppeteerLaunch,
 	LaunchOptions,
